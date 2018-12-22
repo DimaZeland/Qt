d9b5cb298bb8788b58d176d8aa66206b0ea27f74
Dima Zeland <onenewinvest@gmail.com>
2018-12-22 13:38:37 +0200

    Add files via upload

---
+++ Max Schlee Qt 5.10/Chapter 6. Controlling the automatic placement of elements/Listing 6.4 Nested placement of two layout managers/main.cpp
@@ -0,0 +1,39 @@
+// ======================================================================
+//  main.cpp
+// ======================================================================
+//                   This file is a part of the book
+//             "Qt 5.10 Professional programming with C++"
+//                       http://qt-book.com
+// ======================================================================
+//  Copyright (c) 2017 by Max Schlee
+// ======================================================================
+
+#include <QtWidgets>
+
+// ----------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    QWidget      wgt;
+
+    QPushButton* pcmdA = new QPushButton ("А") ;
+    QPushButton* pcmdB = new QPushButton ("В");
+    QPushButton* pcmdC = new QPushButton ("С");
+    QPushButton* pcmdD = new QPushButton ( "D") ;
+    QVBoxLayout* pvbxLayout = new QVBoxLayout;
+    QHBoxLayout* phbxLayout = new QHBoxLayout;
+    phbxLayout->setContentsMargins(5, 5, 5, 5);
+    phbxLayout->setSpacing(15);
+    phbxLayout->addWidget(pcmdC);
+    phbxLayout->addWidget(pcmdD);
+    pvbxLayout->setContentsMargins(5, 5, 5, 5);
+    pvbxLayout->setSpacing(15);
+    pvbxLayout->addWidget(pcmdA);
+    pvbxLayout->addWidget(pcmdB);
+    pvbxLayout->addLayout(phbxLayout);
+    wgt.setLayout(pvbxLayout);
+
+    wgt.show();
+
+    return app.exec();
+}
