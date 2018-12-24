4812725addfe1691ad5f35c28ee7ff1e00cae45d
Dima Zeland <onenewinvest@gmail.com>
2018-12-24 14:18:00 +0200

    Add files via upload

--- Max Schlee Qt 5.10/Chapter 8 Buttons- Flags and Switches/Listing 8.3 Creating a button with a popup menu/main.cpp
+++ Max Schlee Qt 5.10/Chapter 8 Buttons- Flags and Switches/Listing 8.3 Creating a button with a popup menu/main.cpp
@@ -15,21 +15,21 @@ int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
 
-    QWidget    wgt;
-    QCheckBox* pchkNormal = new QCheckBox("&Normal Check Box");
-    pchkNormal->setChecked(true);
+    QGroupBox gbx("&Colors");
 
-    QCheckBox* pchkTristate = new QCheckBox("&Tristate Check Box");
-    pchkTristate->setTristate(true);
-    pchkTristate->setCheckState(Qt::PartiallyChecked);
+    QRadioButton* pradRed   = new QRadioButton("&Red");
+    QRadioButton* pradGreen = new QRadioButton("&Green");
+    QRadioButton* pradBlue  = new QRadioButton("&Blue");
+    pradGreen->setChecked(true);
 
     //Layout setup
     QVBoxLayout* pvbxLayout = new QVBoxLayout;
-    pvbxLayout->addWidget(pchkNormal);
-    pvbxLayout->addWidget(pchkTristate);
-    wgt.setLayout(pvbxLayout);
+    pvbxLayout->addWidget(pradRed);
+    pvbxLayout->addWidget(pradGreen);
+    pvbxLayout->addWidget(pradBlue);
+    gbx.setLayout(pvbxLayout);
 
-    wgt.show();
+    gbx.show();
 
     return app.exec();
 }
