29a2ae0786457f62646e84802acec5f8a4d86044
Dima Zeland <onenewinvest@gmail.com>
2018-12-27 19:41:12 +0200

    Update main.cpp

--- Max Schlee Qt 5.10/Chapter 11  Items of choice/Listing 11.1 Creating a simple list/main.cpp
+++ Max Schlee Qt 5.10/Chapter 11  Items of choice/Listing 11.1 Creating a simple list/main.cpp
@@ -1,7 +1,7 @@
 // ======================================================================
 //  main.cpp
 // ======================================================================
-//                   This file is a part of the book
+//                   This file is a part of the book 
 //             "Qt 5.10 Professional programming with C++"
 //                       http://qt-book.com
 // ======================================================================
@@ -14,22 +14,17 @@
 int main(int argc, char** argv)
 {
     QApplication     app(argc, argv);
+    QStringList      lst;
     QListWidget      lwg;
     QListWidgetItem* pitem = 0;
-    QStringList      lst;
 
     lwg.setIconSize(QSize(48, 48));
-    lwg.setSelectionMode(QAbstractItemView::MultiSelection);
-    lwg.setViewMode(QListView::IconMode);
     lst << "Linux" << "Windows" << "MacOSX" << "Android";
     foreach(QString str, lst) {
-        pitem = new QListWidgetItem(str, &lwg);
+        pitem = new QListWidgetItem(str, &lwg);    
         pitem->setIcon(QPixmap(":/" + str + ".jpg"));
-        pitem->setFlags(Qt::ItemIsEnabled | Qt::ItemIsSelectable |
-                        Qt::ItemIsEditable | Qt::ItemIsDragEnabled);
-
     }
-    lwg.resize(150, 150);
+    lwg.resize(125, 175);
     lwg.show();
 
     return app.exec();
