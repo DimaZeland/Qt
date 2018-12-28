501ad3fc2f14a9556a262cffb30656f9e5acba85
Dima Zeland <onenewinvest@gmail.com>
2018-12-28 14:59:18 +0200

    Add files via upload

---
+++ Max Schlee Qt 5.10/Chapter 11  Items of choice/Listing 11.5 Overwriting QTreeWidgetItem-operator/main.cpp
@@ -0,0 +1,52 @@
+// ======================================================================
+//  main.cpp
+// ======================================================================
+//                   This file is a part of the book
+//             "Qt 5.10 Professional programming with C++"
+//                       http://qt-book.com
+// ======================================================================
+//  Copyright (c) 2017 by Max Schlee
+// ======================================================================
+
+#include <QtWidgets>
+
+// ----------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    QTreeWidget  twg;
+    QStringList  lst;
+
+    lst << "Folders" << "Used Space";
+    twg.setHeaderLabels(lst);
+    twg.setSortingEnabled(true);
+
+    QTreeWidgetItem* ptwgItem = new QTreeWidgetItem(&twg);
+    ptwgItem->setText(0, "Local Disk(C)");
+    ptwgItem->setIcon(0, QPixmap(":/drive.bmp"));
+
+    QTreeWidgetItem* ptwgItemDir = 0;
+    for (int i = 1; i < 20; ++i) {
+        ptwgItemDir = new QTreeWidgetItem(ptwgItem);
+        ptwgItemDir->setText(0, "Directory" + QString::number(i));
+        ptwgItemDir->setText(1, QString::number(i) + "MB");
+        ptwgItemDir->setIcon(0, QPixmap(":/folder.bmp"));
+    }
+    ptwgItem->setExpanded(true);
+    twg.resize(350, 310);
+    twg.show();
+
+    return app.exec();
+}
+
+
+bool MyТreeWidgetItem::operator(const QTreeWidgetItem& ptwiOther)
+{
+bool bRet = false;
+int nColumn = treeWidget()->sortColumn();
+if (nColumn == 0) {
+QString strFormat = "dd.ММ.уууу";
+bRet = QDate::fromString(text(nColumn)) < QDate::fromString(ptwi.text(nColumn) );
+}
+return bRet;
+}
